bad88122880874df8e053ba4a2e62a7a8bca0df5
ankit2001 <ankitchoudhary202.ac@gmail.com>
2021-08-19 20:32:57 +0530

    Update Admin site and register routes for dashboard access

--- fetch_api/admin.py
+++ fetch_api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.Video)
+class VideoAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in models.Video._meta.fields]
+	search_fields = ('published_at', 'vid_id', 'title', 'description'),
+	list_filter = ('published_at', 'title', 'description')
+
+
+@admin.register(models.VideoMetaData)
+class VideoThumbNailAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in models.VideoMetaData._meta.fields]
+	search_fields = ('video__title', 'video__vid_id', 'video__published_at', 'video__description')
+	list_filter = ('current_video', 'thumbnail_size')
+

4057eaa2d460b48e2bcd5302daf5c291381b1d63
ankit2001 <ankitchoudhary202.ac@gmail.com>
2021-08-19 21:33:52 +0530

    Add Comments

--- fetch_api/background_processes.py
+++ fetch_api/background_processes.py
@@ -6,16 +6,20 @@ from django.db import connections
 from collections import deque
 
 
-API_KEYS = open('fetch_api/keys.txt').read().split('\n')
+## Using Stack to optimise the time for taking a key
+API_KEYS = open('fetch_api/example_keys.txt').read().split('\n')
 API_KEYS_STACK = deque()
 
 YOUTUBE_API_SERVICE = "youtube"
 YOUTUBE_API_VERSION = "v3"
+
+# Invterval after which the background process again continues
 INTERVAL_TIME_FOR_PROCESS = 10
 
 for key in API_KEYS:
     API_KEYS_STACK.append(key)
 
+# Dictionary to store the video ids for the videos so that no duplicate exist in api response
 def video_ids_set():
     ids_set = {}
     for data in models.Video.objects.all().iterator():
@@ -23,8 +27,9 @@ def video_ids_set():
     return ids_set
     
 
-def get_response_with_automated_key_optimisation():
 
+def get_response_with_automated_key_optimisation():
+    # Checking a call can be made through any key or not and then deleting the key which is exhausted
     while not API_KEYS_STACK.empty():
         key = API_KEYS_STACK.pop()
         try:
@@ -37,6 +42,8 @@ def get_response_with_automated_key_optimisation():
             continue
     return (None, None)
 
+
+# Background process for fetching the videos from youtube and storing in our database
 @background()
 def schelude_jobs():
     video_set = video_ids_set()
@@ -52,6 +59,7 @@ def schelude_jobs():
             return
         for fetched_video in json_response["items"]:
             try:
+                # Storing the video model after fetching throungh youtube
                 vid_id = fetched_video['id']['videoId']
                 if (video_set.get(vid_id) == None):
                     Id = fetched_video['id']['videoId']
@@ -63,6 +71,7 @@ def schelude_jobs():
                     Thumbnail_url = Thumbnail['url']
                     video_model = models.Video(vid_id=Id, title=Title, description=Description, published_at=Published_at, thumbnail_url=Thumbnail_url)
                     video_model.save()
+                    # Storing the meta data associated with video in separte table
                     meta_datas = [{'thumbnail_size': thumbnail_size, 'thumbnail_url': Snippet['thumbnails'][thumbnail_size]['url'],} for thumbnail_size in Snippet['thumbnails']]
                     for meta_data in meta_datas:
                         meta_data['current_video'] = video_model

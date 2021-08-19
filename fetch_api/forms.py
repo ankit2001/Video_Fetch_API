ea3edbc359ba69e729d2328d003a59934e565a82
ankit2001 <ankitchoudhary202.ac@gmail.com>
2021-08-19 20:27:52 +0530

    Add serializers

--- fetch_api/forms.py
+++ fetch_api/forms.py
@@ -1,2 +1,25 @@
 from rest_framework import serializers
-from .models import models
+
+from . import models
+from . import background_processes
+
+class VideoSerializer(serializers.ModelSerializer):
+    total_meta_data = serializers.SerializerMethodField()
+
+    def get_total_meta_data(self, obj):
+        return [VideoMetaDataSerializer(meta_data).data for meta_data in models.VideoMetaData.objects.filter(current_video=obj)]
+
+    class Meta:
+        model = models.Video
+        fields = '__all__'
+        read_only_fields = ('vid_id',)
+
+class VideoMetaDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.VideoMetaData
+        fields = '__all__'
+
+class VideoAutoCompSer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Video
+        fields = ('vid_id', 'description', 'title',)
